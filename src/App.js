8825d690fee0067def59f830169466f93a7a5d00
Alexander John <96aljohn@gmail.com>
2024-09-01 17:29:34 -0700

    create-form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+const businesses = [
+  {
+    businessId: "b1",
+    name: "San Mateo Public Library",
+    address: "55 W 3rd Ave",
+    category: "Library",
+  },
+  {
+    businessId: "b2",
+    name: "Ducky's Car Wash",
+    address: "716 N San Mateo Dr",
+    category: "Car Wash",
+  },
+  {
+    businessId: "b3",
+    name: "Hanabi",
+    address: "723 California Dr",
+    category: "Restaurant",
+  },
+];
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Business Search</h1>
+      <form>
+        <label>
+          Select Business Category:
+          <select
+            value="All">
+            <option value="All">All</option>
+            <option value="Library">Library</option>
+            <option value="Restaurant">Restaurant</option>
+            <option value="Car Wash">Car Wash</option>
+          </select>
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+
+      <h2>Results</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Address</th>
+            <th>Category</th>
+          </tr>
+        </thead>
+        <tbody>
+          {businesses.map((b, i) => (
+            <tr key={i}>
+              <td>{b.name}</td>
+              <td>{b.address}</td>
+              <td>{b.category}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
